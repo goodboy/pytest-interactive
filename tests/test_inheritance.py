af8b9fc25db5ee78387d39d240f5a81f6bcaae0c
Tyler Goodlet <tgoodlet@gmail.com>
2014-07-25 20:03:02 -0400

    add some more tests to the tree

---
+++ tests/test_inheritance.py
@@ -0,0 +1,47 @@
+# test set for inheritance bug
+import pytest
+import ipdb
+
+# thanks to:
+# http://stackoverflow.com/questions/11281698/python-dynamic-class-methods
+class HookMeta(type):
+    def __init__(cls, name, bases, dct):
+        super(HookMeta, cls).__init__(name, bases, dct)
+
+# from types import MethodType
+    # def __getattr__(cls, attr):
+        # print('getting!')
+        # print(HookMeta.old_set)
+        # if attr is '__setattr__':
+        #     print('got setattr!')
+        #     def set_hook(cls, key, value):
+        #         ipdb.set_trace()
+        #         # old_set(cls, key, value)
+        #     # setattr(cls, attr, classmethod(set_hook))
+        #     return MethodType(set_hook, cls, cls.__metaclass__)
+        #     # return classmethod(set_hook)#getattr(cls, attr)
+        # return super(HookMeta, cls).__getattribute__(attr)
+
+    def __setattr__(cls, attr, value):
+        print("attr: '{}', value: '{}'".format(attr, value))
+        print("class: '{}'".format(cls))
+        # ipdb.set_trace()
+        return super(HookMeta, cls).__setattr__(attr, value)
+
+
+class TestBase(object):
+    __metaclass__ = HookMeta
+
+    def test_cls(self):
+        assert 1
+
+@pytest.fixture
+def doggy():
+    return 'doggy'
+
+@pytest.mark.usefixtures('doggy')
+class TestChild(TestBase):
+    pass
+
+class TestSibling(TestBase):
+    pass
---
+++ tests2/test_set_root2.py
@@ -0,0 +1,53 @@
+# test set for plugin testing
+# from IPython import embed
+import ipdb
+import pytest
+
+class Dut(object):
+    'fake a device under test'
+
+    _allowed = ('a', 'b', 'c')
+
+    def __init__(self, mode=None):
+        self._mode = mode
+
+    def get_mode(self):
+        return self._mode
+
+    def set_mode(self, val):
+        self._mode = val
+
+    def check_mode(self):
+        assert self._mode in self._allowed
+
+# fixtures
+@pytest.fixture
+def dut(request):
+    return Dut('c')
+
+
+@pytest.yield_fixture(params=('a', 'b', 'c'))
+def mode(request, dut):
+    orig_mode = dut.get_mode()
+    dut.set_mode(request.param)
+    yield dut
+    dut.set_mode(orig_mode)
+
+
+@pytest.yield_fixture(params=[1, 2, 3])
+def inputs(request):
+    yield request.param
+
+
+def test_modes(mode):
+    assert mode.check_mode()
+
+
+def test_inputs(inputs):
+    assert inputs < 2
+
+
+class TestBoth(object):
+    def test_m(self, mode, inputs):
+        assert mode.check_mode()
+        assert inputs < 2
