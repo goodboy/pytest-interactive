f07f9ee23e66e18c1c27dd4c7b497ee6dd9fe8a8
Tyler Goodlet <tgoodlet@gmail.com>
2014-09-12 01:09:08 -0400

    add docstring for %show

--- interactive/shell.py
+++ interactive/shell.py
@@ -23,49 +23,73 @@ class PytestShellEmbed(InteractiveShellEmbed):
 class SelectionMagics(Magics):
 
     def ns_eval(self, line):
-        '''Evalutate line in the embedded ns and return it
+        '''Evalutate line in the embedded ns and return result
         '''
         ns = self.shell.user_ns
         return eval(line, ns)
 
     @property
-    def selection(self):
-        return self.ns_eval('tt._selection')
+    def tt(self):
+        return self.ns_eval('tt')
 
     @property
-    def test_tree(self):
-        return self.ns_eval('tt')
+    def selection(self):
+        return self.tt._selection
+
+    @property
+    def tr(self):
+        return self.tt._tr
 
     @line_magic
     def add(self, line):
         '''add tests to the current selection
         '''
-        ts = self.ns_eval(line)
-        if ts:
-            self.selection.addtests(ts)
+        if line:
+            ts = self.ns_eval(line)
+            if ts:
+                self.selection.addtests(ts)
+            else:
+                raise TypeError("'{}' is not a test set".format(ts))
         else:
-            raise TypeError("'{}' is not a test set".format(ts))
+            print("No test set provided?")
 
     @line_magic
     def remove(self, line):
         '''remove tests from the current selection
         '''
-        if ':' in line:
-            return line
+        if self.selection:
+            if ':' in line:
+                return line
+            else:
+                    self.selection.clear()
         else:
-            self.selection.clear()
+            print("No tests currently selected?")
+
         # getter = operator.itemgetter(self.selection, line)
         # self.selection.remove(self.ns_eval(line))
 
     @line_magic
-    def show(self, line):
-        '''show all currently selected test'''
-        if line:
-            ts = self.ns_eval(line)
-            self.test_tree._tprint(ts._items)
+    def show(self, test_set):
+        '''Show all currently selected test by pretty printing
+        to the console.
+
+        With no arguments this command is will display the currentlly selected
+        set of tests. With a test set as an argument it will display all tests
+        in the set.
+
+        Usage:
+
+            show:  print currently selected tests
+
+            show <test_set>:  print all tests in test_set
+        '''
+        if test_set:
+            ts = self.ns_eval(test_set)
+            self.tt._tprint(ts._items)
         else:
             items = self.selection.values()
             if items:
-                self.test_tree._tprint(items)
+                self.tt._tprint(items)
             else:
-                print("No items selected")
+                self.tr.write("error: ", red=True)
+                self.tr.write_line("No tests selected")
