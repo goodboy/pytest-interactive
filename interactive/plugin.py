e93ebe6fba587e3e9274015d2b6c3a4fe0654866
Simon Gomizelj <sgomizelj@sangoma.com>
2014-12-11 19:19:07 -0500

    Our pytest_collection_modifyitems hook should be run last
    Otherwise we have a potential conflict with other plugins using this
    hook, such as pytest-ordering

--- interactive/plugin.py
+++ interactive/plugin.py
@@ -30,6 +30,7 @@ def pytest_configure(config):
         pytest.exit(1)
 
 
+@pytest.mark.trylast
 def pytest_collection_modifyitems(session, config, items):
     """called after collection has been performed, may filter or re-order
     the items in-place.
